6259af503207aa3e40d194161629e2de9cb94cbd
Shivam <ss056989@gmail.com>
2021-10-15 02:44:29 +0530

    css adjustment for main page and navbar

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,19 +1,19 @@
 import React from "react";  
-import './Intro.css';
+import './shivam.css';
 
 export default function Intro() {
     return (
         <div className="home">
-            <div className="left-child">
+            <div className="left child">
                 <h2 className="title">Want to earn while spreading Happiness at the same time ?</h2>
                 <br/>
-                <button>Rent-Out</button>
+                <button className="left-button">Rent-Out</button>
             </div>
             <div className="line"></div>
-            <div className="right-child">
+            <div className="right child">
                 <h2 className="title">Urgent need of something for a while ?</h2>
                 <br/>
-                <button>Rent In</button>
+                <button className="right-button">Rent In</button>
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,29 +2,27 @@ import React,{useState} from 'react'
 import './Navbar.css'
 
 export default function Navbar() {
-    const [tog,setTog] = useState(true);
+    const [tog,setTog] = useState(false);
     return (
         <div className = "Navbar">
             <div className="upper">
                 <div className="uleft">
-                <div className="headlogo"><h1>Padoci</h1></div>
+                    <div className="headlogo"><h1>Padoci</h1></div>
                 </div>
                 <div className="uright">
-                <div className="login"><a href="">Login</a></div>
+                    <div className="login"><a href="">Login</a></div>
                 </div>
             </div>
             <div className="lower">
-            <div className="Left">
-            
-            <div className="links" id={tog ?"hidden":""}> 
-            
-                <a href="/all">All</a>
-                <a href="/frequent">Frequent Rentals</a>
-                <a href="/cars">Cars</a>
-                <a href="/bikes">Bikes</a>
-                <a href="/hardware">Hardware</a>
-            </div>
-            <button onclick = {()=> setTog(!tog)}>Open</button>
+                <div className="Left">
+                <div className="links" id={tog ?"hidden":""}> 
+                    <a href="/all">All</a>
+                    <a href="/frequent">Frequents</a>
+                    <a href="/cars">Cars</a>
+                    <a href="/bikes">Bikes</a>
+                    <a href="/hardware">Hardware</a>
+                </div>
+                <button onClick = {()=> setTog(!tog)}>Open</button>
             </div>
             <div className="right">
                 <input type="text" placeholder = "Search..." />

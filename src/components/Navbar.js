d54c722c434779a7791243cec198c2640d5499f8
thakur0635 <83726506+thakur0635@users.noreply.github.com>
2021-10-19 13:13:42 +0530

    fiirst final animation bachi h wo aati ni hme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
 export default function Navbar() {
-    const [tog,setTog] = useState(false);
+    const [tog, setTog] = useState(false);
+    const [tg, setTg] = useState(false);
     return (
-        <div className = "Navbar">
+        <div className="Navbar">
             <div className="upper">
                 <div className="uleft">
                     <div className="headlogo"><h1>Padoci</h1></div>
@@ -15,20 +16,26 @@ export default function Navbar() {
             </div>
             <div className="lower">
                 <div className="Left">
-                <div className="links" id={tog ?"hidden":""}> 
-                {/* <a href="" className="closebtn" >&times;</a> */}
-                    <a href="/all">All</a>
-                    <a href="/frequent">Frequents</a>
-                    <a href="/cars">Cars</a>
-                    <a href="/bikes">Bikes</a>
-                    <a href="/hardware">Hardware</a>
+                    <div className="links" id={tog ? "hidden" : ""}>
+                        {/* <a href="" className="closebtn" >&times;</a> */}
+                        <a href="/all">All</a>
+                        <a href="/frequent">Frequents</a>
+                        <a href="/cars">Cars</a>
+                        <a href="/bikes">Bikes</a>
+                        <a href="/hardware">Hardware</a>
+                    </div>
+                    <div class="burger" id={tg ? "toggl" : ""} onClick={() => {setTog(!tog);
+                        setTg(!tg)}}>
+                        <div class="line-1"></div>
+                        <div class="line-2"></div>
+                        <div class="line-3"></div>
+                    </div>
+                 
                 </div>
-                <button onClick = {()=> setTog(!tog)}>Open</button>
-            </div>
-            <div className="right">
-                <input type="text" placeholder = "Search..." />
-                <button>Search</button>
-            </div>
+                <div className="right">
+                    <input type="text" placeholder="Search..." />
+                    <button>Search</button>
+                </div>
             </div>
         </div>
     )
